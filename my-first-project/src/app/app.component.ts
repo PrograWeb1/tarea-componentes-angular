dd0702cf537beaef29296e5298723956dea7319f
HaroruDa3 <harol.kock34@gmail.com>
2024-02-29 23:15:38 -0600

    x
    x

---
+++ my-first-project/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,NavbarComponent,HeroComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'my-first-project';
+}
